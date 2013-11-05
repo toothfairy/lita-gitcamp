b582db361e584b5eaf4e73c3fec8fc062748beb5
Sergeev Peter <sergeev.peter@gmail.com>
2013-11-05 18:25:10 +0400

    Start gitcamp handler

--- lib/lita/handlers/gitcamp.rb
+++ lib/lita/handlers/gitcamp.rb
@@ -3,6 +3,18 @@ require "lita"
 module Lita
   module Handlers
     class Gitcamp < Handler
+    	http.post "/gitcamp", :receive
+
+    	def self.default_config(config)
+    		config.rooms = :all
+    		config.token = nil
+    	end
+
+    	def receive(request, responce)
+        if request.params.has_key? :payload
+          push = JSON.parse(request.params[:payload])
+        end # possibly write to log
+    	end
     end
 
     Lita.register_handler(Gitcamp)
--- spec/lita/handlers/gitcamp_spec.rb
+++ spec/lita/handlers/gitcamp_spec.rb
@@ -1,4 +1,56 @@
 require "spec_helper"
 
 describe Lita::Handlers::Gitcamp, lita_handler: true do
+	describe "Gitcamp" do
+		before do 
+      @repo = "https://github.com/octocat/donuts"
+
+      @redis ||= begin
+        redis = Redis.new
+        Redis::Namespace.new("lita.test:handlers:gitcamp", redis: redis)
+      end
+
+      @redis.flushall
+      @redis.set('repos', [@repo])
+    end
+
+    context "Receive github hook" do
+      it "receives json from github" do
+        routes_http(:post, "/gitcamp").to(:receive)
+      end
+	  end
+	end
+
+  describe "receive github payload" do 
+    let(:robot) { double("Lita::Robot") }
+    let(:response) { Rack::Response.new }
+
+    subject {Lita::Handlers::Gitcamp.new(robot)}
+
+    context "with valid data" do 
+      let(:request) do
+        request = double("Rack::Request")
+        request.stub(:params) {{ payload: File.read('./spec/api_data/push_payload.json') }}
+        request
+      end
+
+      it "assepts some github stuff" do
+        subject.receive(request, response)
+        expect(true).to be_true
+      end
+    end
+
+    context "with invalid data" do
+      let(:request) do
+        request = double("Rack::Request")
+        request.stub(:params) {{}}
+        request
+      end
+
+      it "ignores invalid payload" do
+        subject.receive(request, response)
+        expect(true).to be_true
+      end
+    end
+  end
 end
